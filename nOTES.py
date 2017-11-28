8c7fd4288bc954a8fcb4fcd0c4a23501c1c9588f
w3jy <34071936+w3jy@users.noreply.github.com>
2017-11-28 10:46:32 -0800

    Added notes 11/27/17

--- nOTES.py
+++ nOTES.py
@@ -1,1 +1,45 @@
 #  Is this working
+
+print("Hello World")
+print("Welcome to class")
+
+# This is a comment.
+# I can use this to type in text
+# But it will never run
+'''This is a multi-line comment
+I wanted to type out a whole bunch
+but this is all
+text
+'''
+
+# Variables
+a=5
+b=4
+str1="The quick brown fox jumps over the lazy dog"
+
+print(str1)
+
+print(a+b)
+print(a-5*b)
+print(a*b)
+print(a/b)
+print(a**b)   # A to the power of B
+
+
+print(15 % 5)
+print(14 % 5)
+print(13 % 5)
+print(12 % 5)
+print(11 % 5)
+print(10 % 5)
+print(9 % 5)
+print(5 % 3)
+
+
+car_name = "Weibe Mobile"
+car_type = "Nissan GTR"
+car_cylinders = 8
+car_mpg = 9000.1
+
+# Inline printing
+print("I have a car called the %s" The GTR)
